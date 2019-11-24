4fdfb979e0ff4153254c1f0218966d1b4199fc6b
szenadam <szenadam1@gmail.com>
2019-11-24 17:37:10 +0100

    Add file lister test

--- src/lister.py
+++ src/lister.py
@@ -1,12 +1,9 @@
 import os
 
-class ListFiles():
+class FileLister():
     def list_files(self, path):
         result = list()
-
-        # r=root, d=directories, f = files
         for root, dir, file in os.walk(path):
             for f in file:
                 result.append(os.path.join(root, f))
-
         return result
--- tests/test_lister.py
+++ tests/test_lister.py
@@ -1,10 +1,21 @@
-import unittest
-from unittest import mock 
-from src.lister import ListFiles
+import unittest, os
+from unittest import mock
+from src.lister import FileLister
 
 
 class TestLister(unittest.TestCase):
     def test_file_lister_exists(self):
-        l = ListFiles()
-        self.assertIsInstance(l, ListFiles)
+        l = FileLister()
+        self.assertIsInstance(l, FileLister)
 
+    def test_lists_files(self):
+        l = FileLister()
+        
+        expected = [os.path.join('.', 'foo'), os.path.join('.', 'bar'), os.path.join('.', 'baz')]
+        result = []
+
+        with mock.patch('os.walk') as mockwalk:
+            mockwalk.return_value = [('.', None, ('foo', 'bar', 'baz'))]
+            result = l.list_files('.')
+
+        self.assertListEqual(result, expected)
