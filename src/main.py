13d7c426cd3baa7ca950c15226447e3dc7693f41
szenadam <szenadam1@gmail.com>
2019-11-27 20:44:39 +0100

    Add argument parser

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='ASCIIfy path.')
+
+parser.add_argument('path', metavar='path', type=str, nargs='+',
+                    help='A Directory path')
+
+args = parser.parse_args()
