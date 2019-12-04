af86f6d2ae11fd631c2c201daa7718a837f9a95b
szenadam <szenadam1@gmail.com>
2019-12-04 12:35:00 +0100

    Rename variable that shadows build-in function

--- src/lister.py
+++ src/lister.py
@@ -4,7 +4,7 @@ import os
 class FileLister:
     def list_files(self, path):
         result = list()
-        for root, dir, file in os.walk(path):
+        for root, dirs, file in os.walk(path):
             for f in file:
                 result.append(os.path.join(root, f))
         return result
