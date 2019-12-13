8cb3410747cccb7b764c20e1f7545dd22884fdd9
szenadam <szenadam1@gmail.com>
2019-12-13 15:22:00 +0100

    Add 'recursive' flag to FileLister methods

--- modules/lister.py
+++ modules/lister.py
@@ -2,17 +2,16 @@ import os
 
 
 class FileLister:
-    def list_files(self, path):
+    def list_files(self, path, recursive=False):
         result = list()
         for root, dirs, file in os.walk(path):
             for f in file:
                 result.append(os.path.join(root, f))
         return result
 
-    def list_dirs(self, path):
+    def list_dirs(self, path, recursive=False):
         result = list()
         for root, dirs, file in os.walk(path):
-            print(dirs)
             for d in dirs:
                 result.append(os.path.join(root, d))
         return result
