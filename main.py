4e826ffb8396297af7d91331c63fb865b1196f7c
szenadam <szenadam1@gmail.com>
2019-12-13 15:25:00 +0100

    Add lister to main method, list files in given path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+from modules.lister import FileLister
 
 
 def main():
@@ -6,13 +7,15 @@ def main():
         ASCIIfy path. Remove non-ascii characters from file or directory names. 
     """
 
+    # Parse arguments
     parser = argparse.ArgumentParser(description=description)
-
     parser.add_argument('path', metavar='path', type=str, nargs='+', help='A Directory path')
-
     args = parser.parse_args()
 
-    print(args.accumulate)
+    # List files in directory
+    file_lister = FileLister()
+
+    print(file_lister.list_files(args.path[0]))
 
 
 if __name__ == "__main__":
