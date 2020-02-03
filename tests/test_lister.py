7ce964ce7dca78646166d7ebdb1b62b5742641ea
szenadam <szenadam1@gmail.com>
2020-02-03 18:00:00 +0100

    Fix PyCharm suggestions in TestLister

--- tests/test_lister.py
+++ tests/test_lister.py
@@ -1,36 +1,36 @@
-import unittest, os
+import os
+import unittest
 from unittest import mock
+
 from modules.lister import FileLister
 
 
 class TestLister(unittest.TestCase):
     def test_file_lister_exists(self):
         # Arrange, Act
-        l = FileLister()
+        lister = FileLister()
         # Assert
-        self.assertIsInstance(l, FileLister)
+        self.assertIsInstance(lister, FileLister)
 
     def test_lists_files(self):
         # Arrange
-        l = FileLister()
+        lister = FileLister()
         expected = [os.path.join('.', i) for i in ['foo', 'bar', 'baz']]
-        result = []
         # Act
         with mock.patch('os.walk') as mockwalk:
             mockwalk.return_value = [('.', None, ('foo', 'bar', 'baz'))]
-            result = l.list_files('.')
+            result = lister.list_files('.')
         # Assert
         self.assertListEqual(result, expected)
-    
+
     @unittest.skip("need to fix os.walk mock")
     def test_list_dirs(self):
         # Arrange
-        l = FileLister()
+        lister = FileLister()
         expected = [os.path.join('.', 'foo'), os.path.join('.', 'bar'), os.path.join('.', 'baz')]
-        result = []
         # Act
         with mock.patch('os.walk') as mockwalk:
-            mockwalk.return_value = [('.', ('foo'), ())]
-            result = l.list_dirs('.')
+            mockwalk.return_value = [('.', 'foo', ())]
+            result = lister.list_dirs('.')
         # Arrange
         self.assertListEqual(result, expected)
